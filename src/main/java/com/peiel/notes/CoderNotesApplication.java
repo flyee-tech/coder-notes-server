16eb4be4d7565a8c8ac1d6315f30c0dc5f3f99e4
peierlong <peielcn@gmail.com>
2020-06-08 16:20:38 +0800

    first commit

---
+++ src/main/java/com/peiel/notes/CoderNotesApplication.java
@@ -0,0 +1,13 @@
+package com.peiel.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peiel/notes/CoderNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peiel.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
