f6646c3190af71a7384e13b4b1995a42913ca756
peierlong <peielcn@gmail.com>
2020-06-11 15:21:03 +0800

    feat: 标签相关接口的处理

--- src/main/java/com/peiel/notes/controller/ArticleController.java
+++ src/main/java/com/peiel/notes/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.peiel.notes.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.peiel.notes.automation.mapper.ArticleMapper;
 import com.peiel.notes.automation.mapper.ArticleTagMapper;
@@ -69,8 +70,8 @@ public class ArticleController {
         return Util.jsonSuccess(map);
     }
 
-    @GetMapping("getPublicListByTag")
-    public JSON getPublicListByTag(String pn, String tid) {
+    @GetMapping("getListByTag")
+    public JSON getListByTag(String pn, String tid, Integer idx) {
         int pageSize = 10;
         int currentPage = pn != null && pn.trim().length() > 0 ? Integer.parseInt(pn) : 1;
         int pageNum = pageSize * (currentPage - 1);
@@ -79,17 +80,15 @@ public class ArticleController {
         List<ArticleTag> ats = articleTagMapper.selectList(Wrappers.lambdaQuery(new ArticleTag()).eq(ArticleTag::getStatus, 1).eq(ArticleTag::getTagId, tid));
         List<Integer> articleIds = ats.stream().map(ArticleTag::getArticleId).collect(Collectors.toList());
 
-        List<Article> list = articleMapper.selectList(Wrappers.lambdaQuery(new Article())
+        LambdaQueryWrapper<Article> wrapper = Wrappers.lambdaQuery(new Article())
                 .in(Article::getId, articleIds)
-                .eq(Article::getStatus, 1)
-                .eq(Article::getIsPublic, 1)
-                .orderByDesc(Article::getId)
-                .last("LIMIT " + pageNum + "," + pageSize));
+                .eq(Article::getStatus, 1);
+        if (idx != null && idx == 1) {
+            wrapper = wrapper.eq(Article::getIsPublic, 1);
+        }
+        List<Article> list = articleMapper.selectList(wrapper.orderByDesc(Article::getId).last("LIMIT " + pageNum + "," + pageSize));
 
-        Integer count = articleMapper.selectCount(Wrappers.lambdaQuery(new Article())
-                .in(Article::getId, articleIds)
-                .eq(Article::getStatus, 1)
-                .eq(Article::getIsPublic, 1));
+        Integer count = articleMapper.selectCount(wrapper);
         ModelMap map = new ModelMap();
         map.put("list", list);
         map.put("count", count);
@@ -101,7 +100,9 @@ public class ArticleController {
     public JSON getDetail(Integer id) {
         Article article = articleMapper.selectById(id);
         List<Integer> ids = articleTagMapper.selectList(
-                Wrappers.lambdaQuery(new ArticleTag()).eq(ArticleTag::getArticleId, id))
+                Wrappers.lambdaQuery(new ArticleTag())
+                        .eq(ArticleTag::getArticleId, id)
+                        .eq(ArticleTag::getStatus, 1))
                 .stream().map(ArticleTag::getTagId).collect(Collectors.toList());
         ModelMap map = new ModelMap();
         if (ids.size() != 0) {
@@ -119,13 +120,31 @@ public class ArticleController {
     }
 
     @PostMapping("save")
-    public JSON save(Article article) {
+    public JSON save(Article article, String tags) {
         article.setContent(URLDecoder.decode(article.getContent()));
         if (article.getId() == null) {
             articleMapper.insert(article);
         } else {
             articleMapper.updateById(article);
         }
+
+        if (tags != null) {
+            articleTagMapper.update(ArticleTag.builder().status(0).build(), Wrappers.lambdaQuery(new ArticleTag())
+                    .eq(ArticleTag::getArticleId, article.getId()));
+            String[] ts = tags.split(" ");
+            for (String tag : ts) {
+                tag = tag.replace("#", "").trim();
+                // 查看标签库是否存在标签
+                Tag t = tagMapper.selectOne(Wrappers.lambdaQuery(new Tag()).eq(Tag::getName, tag));
+                if (t == null) {
+                    t = Tag.builder().name(tag).build();
+                    tagMapper.insert(t);
+                }
+                // 建立文章和标签的关联关系
+                articleTagMapper.insert(ArticleTag.builder().articleId(article.getId()).tagId(t.getId()).build());
+            }
+        }
+
         ModelMap map = new ModelMap();
         map.put("id", article.getId());
         return Util.jsonSuccess(map);
